a0261440cae190dcee11a9a5f106d7ba5f4a10ba
Nqobile Buthelezi <nqobilethabobuthelezi@gmail.com>
2025-03-20 13:50:16 +0200

    [Feature]: back end read, update and delete task functionality added.

--- src/js/app.js
+++ src/js/app.js
@@ -3,4 +3,19 @@ function addTask( tasks, newTask )
     return [...tasks, newTask];
 }
 
-module.exports = { addTask };
+function removeTask(tasks, taskIdToRemove)
+{
+    return tasks.filter( task => task.id !== taskIdToRemove );
+}
+
+function toggleTaskCompletion(tasks, taskIdToRemove)
+{
+    return tasks.map( task => {
+        if ( task.id === taskIdToRemove ) {
+            return { ...task, completed: !task.completed };
+        }
+        return task;
+    });
+}
+
+module.exports = { addTask, removeTask, toggleTaskCompletion };
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,15 +1,40 @@
-const { addTask } = require( "../src/js/app" );
+const { addTask, removeTask, toggleTaskCompletion } = require( "../src/js/app" );
 
 describe( "Task Management", () => {
 
-    test( "Should add a new task", () => {
-        const tasks = [];
-        const newTask = { id: 1, title: "Test Task", completed: false };
+    let tasks;
+
+    beforeEach( () => {
+        tasks = [ { id: 1, title: "Test Task", completed: false } ];
+    })
+
+    test( "Should add a new task.", () => {
+        const newTask = { id: 2, title: "Second Test Task", completed: false };
 
         const updatedTasks = addTask( tasks, newTask );
 
+        expect( updatedTasks.length ).toBe( 2 );
+        expect( updatedTasks[ 1 ] ).toEqual( newTask );
+    });
+
+    test( "Should be able to read the title.", () => {
+        expect( tasks[ 0 ].title ).toBe( "Test Task" );
+    });
+
+    test("Should toggle task completion status", () => {
+        const taskIdToToggle = 1;
+        const updatedTasks = toggleTaskCompletion( tasks, taskIdToToggle );
+        
         expect( updatedTasks.length ).toBe( 1 );
-        expect( updatedTasks[ 0 ] ).toEqual( newTask );
+        expect( updatedTasks[ 0 ].completed ).toBe( true );
+    });
+
+    test( "Should remove a task", () => {
+        const taskIdToRemove = 1;
+
+        const updatedTasks = removeTask( tasks, taskIdToRemove );
+
+        expect( updatedTasks.length ).toBe( 0 );
     });
 
 });
